bdc4885352f9354dbe23b21930a25d00677408ab
Inf1n1ty75 <volkandu75@gmail.com>
2023-12-09 15:57:14 +0100

    reorganizing main and create list

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: vopekdas <vopekdas@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/11/07 14:36:07 by vopekdas          #+#    #+#              #
-#    Updated: 2023/12/09 14:45:21 by vopekdas         ###   ########.fr        #
+#    Updated: 2023/12/09 15:55:01 by vopekdas         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -17,6 +17,7 @@ SOURCES =  instruction_push.c \
 			instruction_rotate.c \
 			instruction_reverse_rotate.c \
 			parsing.c \
+			main.c \
 
 LIBFT_PATH = Libft
 
--- create_list_argv.c
+++ create_list_argv.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vopekdas <vopekdas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/09 15:08:40 by vopekdas          #+#    #+#             */
-/*   Updated: 2023/12/09 15:55:12 by vopekdas         ###   ########.fr       */
+/*   Created: 2023/12/09 14:32:33 by vopekdas          #+#    #+#             */
+/*   Updated: 2023/12/09 15:11:26 by vopekdas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,35 @@
 #include "ft_printf/include/ft_printf.h"
 #include "push_swap.h"
 
-int	main(int	argc, char **argv)
+t_list	*ft_linked_list(int argc, char **argv)
 {
 	int		i;
+	t_list	*new_node;
 	t_list	*new_list;
 
-	if (argc <= 1)
-		return (1);
 	i = 1;
-	new_list = ft_linked_list(argc, argv);
-	return (0);
+	new_list = NULL;
+	while (i < argc)
+	{
+		new_node = ft_lstnew(ft_atoi(argv[i]));
+		ft_lstadd_back(&new_list, new_node);
+		i++;
+	}
+	return (new_list);
 }
+// int	main(int	argc, char **argv)
+// {
+// 	int		i;
+// 	t_list	*new_list;
+// 	t_list	*current;
+
+// 	i = 1;
+// 	new_list = ft_linked_list(argc, argv);
+// 	current = new_list;
+// 	while (current)
+// 	{
+// 		ft_printf("%d -> ", current->content);
+// 		current = current->next;
+// 	}
+// 	return (0);
+// }
