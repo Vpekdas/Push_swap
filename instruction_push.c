a03f95e5f30a1306763101a64221b025abb669f7
Inf1n1ty75 <volkandu75@gmail.com>
2023-12-12 17:28:13 +0100

    need to recode push instruct

--- instruction_push.c
+++ instruction_push.c
@@ -6,7 +6,7 @@
 /*   By: vopekdas <vopekdas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 16:58:57 by vopekdas          #+#    #+#             */
-/*   Updated: 2023/12/12 13:11:04 by vopekdas         ###   ########.fr       */
+/*   Updated: 2023/12/12 17:25:53 by vopekdas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,14 @@
 
 void	ft_instruction_pa(t_list **list, t_list **list2)
 {
-	if (!list)
-		return ;
-	ft_lstadd_front(list, *(list2));
+	ft_lstadd_front(list, (*list2));
 	ft_printf("pa\n") ;
 }
 
 void	ft_instruction_pb(t_list **list, t_list **list2)
 {
-	if (!list)
+	
+	if !(*list)
 		return ;
-	ft_lstadd_front(list, *(list2));
 	ft_printf("pb\n") ;
 }
--- sort_five.c
+++ sort_five.c
@@ -6,7 +6,7 @@
 /*   By: vopekdas <vopekdas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 11:39:40 by vopekdas          #+#    #+#             */
-/*   Updated: 2023/12/12 15:32:48 by vopekdas         ###   ########.fr       */
+/*   Updated: 2023/12/12 17:22:54 by vopekdas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,52 +14,31 @@
 #include "ft_printf/include/ft_printf.h"
 #include "push_swap.h"
 
-int	main () {
-	t_list	*node1 = ft_lstnew(75);
-	t_list	*node2 = ft_lstnew(12);
-	t_list	*node3 = ft_lstnew(90);
-	t_list	*node4 = ft_lstnew(124);
-	t_list	*node5 = ft_lstnew(74);
 
-	t_list *list1 = node1;
-	t_list *list2 = NULL;
-	t_list	*currrent = NULL;
+int	main (int argc, char **argv) {
 
+	t_list	*list1 = NULL;
+	t_list	*list2 = NULL;
+	t_list	*current = NULL;
 
-	node1->next = node2;
-	node2->next = node3;
-	node3->next = node4;
-	node4->next = node5;
-	currrent = list1;
+	list1 = ft_linked_list(argc, argv);
 
-	ft_printf("LINK\n");
-	while (currrent)
+	current = list1;
+	while(current)
 	{
-		ft_printf("%d -> ", currrent->content);
-		currrent = currrent->next;
+		ft_printf("%d -> ", current->content);
+		current = current->next;
 	}
-	ft_printf("%s\n", currrent);
+	ft_printf("%s\n", current);
 
-	ft_instruction_pb(&list2, &node1);
-	
-	currrent = node2;
-	ft_printf("FIRST LINK\n");
-	while (currrent)
+	ft_instruction_pb(&list2, &list1);
+
+	current = list2;
+	while(current)
 	{
-		ft_printf("%d -> ", currrent->content);
-		currrent = currrent->next;
+		ft_printf("%d -> ", current->content);
+		current = current->next;
 	}
-	ft_printf("%s\n", currrent);
-
-	currrent = node1;
-	ft_printf("SECOND LINK\n");
-	while (currrent)
-	{
-		ft_printf("%d -> ", currrent->content);
-		currrent = currrent->next;
-	}
-	ft_printf("%s\n", currrent);
-
-
+	ft_printf("%s\n", current);
 
 }
