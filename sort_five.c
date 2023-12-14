ca8dfa9b64ea37206b7000b7424d99a28c5ad123
Inf1n1Ty_75 <volkandu75@gmail.com>
2023-12-14 01:56:34 +0100

    function to return a pos, to call multiple time ra-rra, to which pos should go added

--- sort_five.c
+++ sort_five.c
@@ -14,6 +14,60 @@
 #include "ft_printf/include/ft_printf.h"
 #include "push_swap.h"
 
+int	ft_num_pos(t_list **stack_a, int num)
+{
+	int	pos;
+
+	pos = 0;
+	while (*stack_a)
+	{
+		if (num == (*stack_a)->content)
+			return (pos);
+		(*stack_a) = (*stack_a)->next;
+		pos++;
+	}
+	return (pos);
+}
+
+void	ft_handle_ra_multiple(t_list **stack_a, int pos)
+{
+	while (pos--)
+		ft_instruction_ra(stack_a);
+}
+
+void	ft_handle_rra_multiple(t_list **stack_a, int pos)
+{
+	while (pos--)
+		ft_instruction_rra(stack_a);
+}
+
+int	ft_which_pos(t_list **stack_a, t_list **stack_b, int pos)
+{
+	t_list	*temp;
+
+	temp = *stack_a;
+
+	while (temp)
+	{
+		pos++;
+		if (*b && (*b)->content > temp->content && (!temp->next || (*b)->content < temp->next->content))
+			return (pos);
+		temp = temp->next;
+	}
+	return (pos);
+}
+
+int	ft_revert_multiple(t_list **stack_a, int range_pos)
+{
+	int	len;
+
+	len = ft_lstsize(*stack_a);
+	if (range_pos < len / 2)
+		(ft_handle_ra_multiple(stack_a, range_pos));
+	else
+		(ft_handle_rra_multiple(stack_a, range_pos));
+}
+
 void	ft_sort_five(t_list	**stack_a, t_list	**stack_b)
 {
 	ft_instruction_pb(stack_a, stack_b);
