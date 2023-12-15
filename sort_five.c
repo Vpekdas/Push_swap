6ca8776cefc8560eaa76606ec1cb9b9d38e18b95
Inf1n1Ty_75 <volkandu75@gmail.com>
2023-12-15 02:04:58 +0100

    sorting five is ok, need to re sort after pa

--- sort_five.c
+++ sort_five.c
@@ -14,99 +14,163 @@
 #include "ft_printf/include/ft_printf.h"
 #include "push_swap.h"
 
-void	ft_ra_multiple(t_list **stack_a, int pos)
+void	ft_ra_multiple(t_list **a, int pos)
 {
-	while (pos--)
-		ft_ra(stack_a);
+	while (pos > 0)
+	{
+		ft_ra(a);
+		pos--;
+	}
 }
 
-void	ft_rra_multiple(t_list **stack_a, int pos)
+void	ft_rra_multiple(t_list **a, int pos)
 {
-	while (pos--)
-		ft_rra(stack_a);
+	int	len;
+
+	len = ft_lstsize(*a);
+	pos = len - pos;
+	while (pos > 0)
+	{
+		ft_rra(a);
+		pos--;
+	}
 }
 
 int	ft_find_biggest(t_list **a)
 {
-	int	num;
+	int		num;
+	t_list	*current;
 
 	num = (*a)->content;
-	while ((*a))
+	current	= (*a);
+	while (current)
 	{
-		if ((*a)->content > num)
-			num = (*a)->content;
-		(*a) = (*a)->next;
+		if (current->content > num)
+			num = current->content;
+		current = current->next;
 	}
 	return (num);
 }
 
 int	ft_find_smaller(t_list **a)
 {
-	int	num;
+	int		num;
+	t_list	*current;
 
 	num = (*a)->content;
-	while ((*a))
+	current	= (*a);
+	while (current)
 	{
-		if ((*a)->content <= num)
-			num = (*a)->content;
-		(*a) = (*a)->next;
+		if (current->content <= num)
+			num = current->content;
+		current = current->next;
 	}
 	return (num);
 }
 
-void	ft_rotate_multiple(t_list **a, int range_pos)
+void	ft_rotate_multiple(t_list **a, int pos)
 {
 	int	len;
 
 	len = ft_lstsize(*a);
-	if (range_pos < len / 2)
-		(ft_ra_multiple(a, range_pos));
+	if (pos < len / 2)
+		(ft_ra_multiple(a, pos));
 	else
-		(ft_rra_multiple(a, range_pos));
+		(ft_rra_multiple(a, pos));
 }
 
-void	ft_sort_five(t_list	**stack_a, t_list	**stack_b)
+int	ft_num_pos(t_list **a, int num)
 {
-	ft_pb(stack_a, stack_b);
-	ft_pb(stack_a, stack_b);
-	ft_sort_three(stack_a);
+	t_list	*current;
+	int		pos;
+
+	current = (*a);
+	pos = 0;
+	while (current)
+	{
+		if (current->content == num)
+			return (pos);
+		current = current->next;
+		pos++;
+	}
+	return (pos);
 }
-// int	main (int argc, char **argv)
-// {
-// 	t_list	*list = ft_linked_list(argc, argv);
-// 	t_list	*list2 = NULL;
-// 	t_list	*current = list;
 
-// 	ft_printf("BEFORE SORTING\n");
-// 	ft_printf("==================\n");
-// 	while (current)
-// 	{
-// 		ft_printf("%d -> ", current->content);
-// 		current = current->next;
-// 	}
-// 	ft_printf("%s\n", current);
+int	ft_find_right_pos(t_list **a, t_list **b)
+{
+	int		pos;
+	t_list	*tmp;
+	int		smaller;
+	int		biggest;
 
-// 	ft_sort_five(&list, &list2);
+	smaller = ft_find_smaller(a);
+	biggest = ft_find_biggest(a);
+	tmp = (*a);
+	if ((*b)->content < smaller)
+		return (ft_num_pos(a, smaller));
+	if ((*b)->content > biggest)
+		return (ft_num_pos(a, biggest) + 1);
+	pos = 1;
+	while (tmp)
+	{
+		if ((*b)->content > tmp->content && (*b)->content < tmp->next->content)
+			return (pos);
+		tmp = tmp->next;
+		pos++;
+	}
+	return (pos);
+}
+void	ft_sort_five(t_list	**a, t_list	**b)
+{
+	int	pos;
 
-// 	current = list;
+	pos = 0;
+	ft_pb(a, b);
+	ft_pb(a, b);
+	ft_sort_three(a);
+	pos = ft_find_right_pos(a, b);
+	ft_rotate_multiple(a, pos);
+	ft_pa(b, a);
+	pos = ft_find_right_pos(a, b);
+	ft_rotate_multiple(a, pos);
+	ft_pa(b, a);
+}
+int	main (int argc, char **argv)
+{
+	t_list	*list = ft_linked_list(argc, argv);
+	t_list	*list2 = NULL;
+	t_list	*current = list;
 
-// 	ft_printf("AFTER SORTING IN STACK A\n");
-// 	ft_printf("==================\n");
-// 	while (current)
-// 	{
-// 		ft_printf("%d -> ", current->content);
-// 		current = current->next;
-// 	}
-// 	ft_printf("%s\n", current);
+	ft_printf("BEFORE SORTING\n");
+	ft_printf("==================\n");
+	while (current)
+	{
+		ft_printf("%d -> ", current->content);
+		current = current->next;
+	}
+	ft_printf("%s\n", current);
 
-// 	current = list2;
+	ft_sort_five(&list, &list2);
 
-// 	ft_printf("AFTER SORTING IN STACK B\n");
-// 	ft_printf("==================\n");
-// 	while (current)
-// 	{
-// 		ft_printf("%d -> ", current->content);
-// 		current = current->next;
-// 	}
-// 	ft_printf("%s\n", current);
-// }
+	current = list;
+
+	ft_printf("AFTER SORTING IN STACK A\n");
+	ft_printf("==================\n");
+	while (current)
+	{
+		ft_printf("%d -> ", current->content);
+		current = current->next;
+	}
+	ft_printf("%s\n", current);
+
+	current = list2;
+
+	ft_printf("AFTER SORTING IN STACK B\n");
+	ft_printf("==================\n");
+	while (current)
+	{
+		ft_printf("%d -> ", current->content);
+		current = current->next;
+	}
+	ft_printf("%s\n", current);
+}
